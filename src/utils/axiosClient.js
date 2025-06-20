7725d2a6099e9fb46a86860bf4040f08118552f5
dung2502 <nguyenducdung704@gmail.com>
2025-06-20 11:15:37 +0700

    update authentication service

--- src/utils/axiosClient.js
+++ src/utils/axiosClient.js
@@ -1,41 +1,36 @@
 import axios from 'axios';
-import {toast} from "react-toastify";
+import { toast } from 'react-toastify';
 
 const apiUrl = process.env.REACT_APP_API_URL;
 
 const axiosClient = axios.create({
-  baseURL: `${apiUrl}/api/auth`,
+    baseURL: `${apiUrl}/api/auth`,  // Ví dụ: https://api.music-streaming-website.blog/api/auth
     withCredentials: true,
 });
 
 axiosClient.interceptors.request.use(
     (config) => {
         const user = JSON.parse(localStorage.getItem("user"));
-        if (user && user.token) {
-            config.headers.Authorization = `Bearer ${user.token}`;
+        if (user?.userId) {
+            config.headers.Userid = user.userId; // BE yêu cầu header này để xử lý rft
         }
-        config.headers.userId = user?.userId;
         return config;
     },
-    (error) => {
-        return Promise.reject(error);
-    }
+    (error) => Promise.reject(error)
 );
 
-
-
 axiosClient.interceptors.response.use(
-  (response) => {
-    return response;
-  },
-  (error) => {
-    if (error.response && error.response.status === 401) {
-      console.error('Lỗi 401: Unauthorized');
-    }else if(error.code === "ERR_NETWORK"){
-      toast.error("Máy chủ đang gặp sự cố !");
+    (res) => res,
+    (err) => {
+        if (err.response?.status === 401) {
+            toast.error("Phiên đăng nhập hết hạn. Vui lòng đăng nhập lại.");
+            localStorage.removeItem("user");
+            window.location.href = "/login";
+        } else if (err.code === "ERR_NETWORK") {
+            toast.error("Không thể kết nối đến máy chủ!");
+        }
+        return Promise.reject(err);
     }
-    return Promise.reject(error);
-  }
 );
 
 export default axiosClient;
